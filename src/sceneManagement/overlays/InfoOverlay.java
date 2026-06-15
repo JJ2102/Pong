84fabd4bde0ce592b0583c3b525b8af0d619e267
JJ2102 <154095093+JJ2102@users.noreply.github.com>
2026-06-15 15:13:26 +0200

    updated Information

--- src/sceneManagement/overlays/InfoOverlay.java
+++ src/sceneManagement/overlays/InfoOverlay.java
@@ -11,13 +11,20 @@ public class InfoOverlay extends Overlay {
     public InfoOverlay(GameWindow window) {
         super(window, "Information", 225, false);
 
-        JLabel controlsLabel = new JLabel( // TODO: add more info (what is the Game about, etc.)
-                "<html>" +
-                        "<div style='text-align: center;'>" +
-                            "Use Mouse to move the paddle.<br>" +
-                            "Press 'ESC' to pause the game.<br>" +
-                        "</div>" +
-                    "</html>");
+        JLabel controlsLabel = new JLabel("""
+    <html>
+        <div style='text-align: center;'>
+            <b>How to Play</b><br><br>
+
+            Move the paddle with your mouse.<br>
+            Block incoming shots and outplay your opponent.<br>
+            Earn points whenever the ball gets past the opponent's paddle.<br>
+            Press <b>ESC</b> to pause the game.<br><br>
+
+            <i>Inspired by the classic Atari game Pong.</i>
+        </div>
+    </html>
+    """);
         controlsLabel.setFont(Globals.getMainFont(24));
         controlsLabel.setForeground(Globals.getFontColor());
         addComponent(controlsLabel);
